6732fea3f23cef83120f2a2c7f145f9d3b39ef90
Calvin <chopin19@byui.edu>
2024-05-01 22:42:25 -0600

    finished assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int userchoice = 1;
+        List<int> numbers = new List<int>();
+
+        while (userchoice != 0)
+        {
+            Console.Write("Enter a number. Press 0 when you are finished: ");
+            userchoice = int.Parse(Console.ReadLine());
+
+            if (userchoice != 0)
+            {
+                numbers.Add(userchoice);
+            }
+
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"Total Sum: {sum}");
+
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"Average: {average}");
+
+        int maximum = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > maximum)
+            {
+                maximum = number;
+            }
+        }
+        Console.WriteLine($"Maximum: {maximum}");
     }
 }
