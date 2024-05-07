9f9531b06bf63fa8a3f4ad41aa7de1290a2558c9
Calvin <chopin19@byui.edu>
2024-05-07 14:30:57 -0600

    fixed errors

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,19 +5,18 @@ class Program
     static void Main(string[] args)
     {
         //using System
-using System;
-using Internal;
 
-//This is equivalent to Python print
-Console.WriteLine("Hello, what is your name?");
 
-//this is equivalent to Python's input function
-string name = Console.ReadLine();
+        //This is equivalent to Python print
+        Console.WriteLine("Hello, what is your name?");
 
-//This is equivalent to Python's f-string
-Console.WriteLine($"Hello, {name}!");
+        //this is equivalent to Python's input function
+        string name = Console.ReadLine();
 
-Console.Write("Enter your age");
-int age = int.Parse(Console.ReadLine());
+        //This is equivalent to Python's f-string
+        Console.WriteLine($"Hello, {name}!");
+
+        Console.Write("Enter your age");
+        int age = int.Parse(Console.ReadLine());
     }
 }
