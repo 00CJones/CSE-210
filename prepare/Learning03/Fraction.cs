e5857bd90be54da69f9d5c0953e81ac54113657b
Calvin <chopin19@byui.edu>
2024-05-23 10:51:40 -0600

    finished

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,54 @@
+using System;
+using System.ComponentModel.Design;
+
+public class Fraction
+{
+    private int top;
+    private int bottom;
+
+    //Constructors
+    public Fraction()
+    {
+        top = 1;
+        bottom = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        top = wholeNumber;
+        bottom = 1;
+    }
+    public Fraction(int Top, int Bottom)
+    {
+        top = Top;
+        bottom = Bottom;
+    }
+
+    //Getters and Setters
+    public int GetTop()
+    {
+        return top;
+    }
+    public void SetTop(int Top)
+        {
+            top = Top;
+        }
+    public int GetBottom()
+    {
+        return bottom;
+    }
+    public void SetBottom(int Bottom)
+    {
+        bottom = Bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string fraction = $"{top}/{bottom}";
+        return fraction;
+    }
+    public double GetDecimalValue()
+    {
+        double Decimal = (double) top / bottom;
+        return Decimal;
+    }
+}
